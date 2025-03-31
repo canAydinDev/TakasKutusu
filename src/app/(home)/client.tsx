6b31d44888239fd4e569544f246cce1a3813ad66
canaydindev <canaydindev@gmail.com>
2025-03-31 16:16:50 +0300

    trpc completed

---
+++ src/app/(home)/client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
+
+export const PageClient = () => {
+  const [data] = trpc.hello.useSuspenseQuery({ text: "Can" });
+
+  return <div>Page client: {data.greeting}</div>;
+};
